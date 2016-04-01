f0975e7e532dd4f194b7a80ec79363c9d0e90d96
Thomas Rhiel <thomas@atavist.net>
2016-04-01 16:47:38 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import SitemakerRouter from './server'
+import SitemakerClient from './client'
+
+export { SitemakerRouter, SitemakerClient }
---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
+import { createPage } from './utils'
+
+function renderSite(req, res, routes) {
+	match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+		if (error) {
+			res.status(500).send(error.message);
+		} else if (redirectLocation) {
+			res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+		} else if (renderProps) {
+			// You can also check renderProps.components or renderProps.routes for
+			// your "not found" component or route respectively, and send a 404 as
+			// below, if you're using a catch-all route.
+
+			res.status(200).send(createPage(renderToString(<RouterContext {...renderProps} />)))
+		} else {
+			res.status(404).send('Not found')
+		}
+	});
+}
+
+var router = express.Router();
+
+function SitemakerRouter(routes) {
+	router.get('*', function(req, res, next) {
+
+		if (typeof routes === 'function') {
+			routes = routes.call(this, req, res);
+		}
+
+		renderSite(req, res, routes);
+	});	
+	return router;
+}
+
+export default SitemakerRouter
